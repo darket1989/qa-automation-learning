0f32497d10b80686cd3f50bd4d4b63fc6a396646
Gerardo Herrera <gherreral1337@gmail.com>
2026-02-06 13:14:42 -0600

    Add playwright project

---
+++ playwright-project/example.spec.js
@@ -0,0 +1,12 @@
+const { test, expect } = require('@playwright/test');
+
+test('homepage has Playwright in title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await expect(page).toHaveTitle(/Playwright/);
+}
+)
+
+
+
+
+
