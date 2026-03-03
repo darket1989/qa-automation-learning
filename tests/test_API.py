b460b38078483cd8217f0060901f9fa581275b01
Gerardo Herrera <gherreral1337@gmail.com>
2026-03-03 16:18:09 -0600

    Update yml file to include API tests

--- tests/test_API.py
+++ tests/test_API.py
@@ -1,26 +1,34 @@
 import pytest
 from playwright.sync_api import *
 
-
+#Create a reusable API client with a default base URL
 @pytest.fixture
 def api_context(playwright: Playwright) -> APIRequestContext:
     api_context = playwright.request.new_context(
         base_url="https://dummyjson.com"
     )
+    # Execution pauses here during the test, once the test finishes, control returns and the lines after yield will run
     yield api_context
     api_context.dispose()
 
 
 def test_users_search(api_context: APIRequestContext):
+    # Target name to search on the response
     query = "Emily"
+    #Perform GEt operation using the base url
     response = api_context.get(f"/users/search?q={query}")
 
+    # Parse response JSON into a python dict
     users_data = response.json()
 
+    # Log how many users were found
     print("Users found:", users_data["total"])
 
+    # Validate each returned user matches the target user name
     for user in users_data["users"]:
         print("Checking user:", user["firstName"])
+
+    # E-nsure the first name contains the search query
         assert query in user["firstName"]
 
 def test_create_user(api_context: APIRequestContext):
